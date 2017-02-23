bf2914f1541218c0005638463d3463149583ec0e
Psychokiller1888 <xXx06ksyadmin>
2017-02-23 10:28:27 +0100

    Version 1.4.9.4 Added some undefined checks and added debug to demo game

--- plugin/files/airconsole/edittime.js
+++ plugin/files/airconsole/edittime.js
@@ -3,7 +3,7 @@ function GetPluginSettings()
   return {
     "name": "AirConsole",
     "id": "AirConsole",
-    "version": "1.4.9.3",
+    "version": "1.4.9.4",
     "description": "Extend your game with local multiplayer fun",
     "author": "N-Dream AG",
     "help url": "http://developers.airconsole.com",
